c503aa71fa962a08a132d6666ad3c5a7a0f74426
abishalih <habibullahshalihuddin@gmail.com>
2023-05-14 23:16:25 +0700

    fix: remove unused var

--- logistic/src/infrastructure/components/EnhancedTable/TableBody/index.jsx
+++ logistic/src/infrastructure/components/EnhancedTable/TableBody/index.jsx
@@ -1,10 +1,6 @@
-import { useContext } from "react";
-import TableContext from "../TableContext";
 import TableBodyRow from "./TableBodyRow";
 
 const TableBody = ({ data = [] }) => {
-    const bodyProps = useContext(TableContext);
-    const { columns = [] } = bodyProps;
     return (
         <thead>
             {data.map((datum, key) => (
