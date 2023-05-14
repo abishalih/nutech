1d3aa1c2518d369b7bb0fece95d83695de9c51a6
abishalih <habibullahshalihuddin@gmail.com>
2023-05-14 17:08:11 +0700

    feature: create global common constants

---
+++ logistic/src/app/common/master.js
@@ -0,0 +1,11 @@
+import IconMaster from "infrastructure/assets/icon-navigation/master.png";
+
+export const FEATURE_MASTER_GOODS = {
+    container: "master/Goods",
+    icon: IconMaster,
+    iconActive: IconMaster,
+    sub: [],
+    title: "Master Barang",
+    url: "/master/goods",
+    withLayout: true
+};
