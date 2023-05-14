4f3e15f391a37330ef7baf58e1f27dcba093a8e8
abishalih <habibullahshalihuddin@gmail.com>
2023-05-14 21:37:44 +0700

    feature: add persist gate and disable temporary

--- logistic/src/app/App.js
+++ logistic/src/app/App.js
@@ -1,33 +1,16 @@
-// import { LIST_FEATURE } from "app/common";
 import PrivateRoute from "app/routes/PrivateRoute";
-// import DynamicApp from "infrastructure/components/common/DynamicApp";
 import ErrorPage from "infrastructure/templates/ErrorPage";
-// import useRouteList from "infrastructure/utils/useRouteList";
 import Home from "pages/Home";
-import MasterGoods from "pages/master/Goods";
+import MasterProduct from "pages/master/Product";
 import { BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-    // const { routeList = [] } = useRouteList(LIST_FEATURE);
     return (
         <BrowserRouter>
             <Switch>
                 <PrivateRoute path="/" component={Home} withLayout exact />
-                <PrivateRoute path="/master/goods" component={MasterGoods} withLayout />
-                {/* {routeList.map(({ url, container, role, withLayout = false }, key) => {
-                    return (
-                        <PrivateRoute
-                            component={() => <DynamicApp componentType="pages" container={container} />}
-                            data-testid="private-route-fok"
-                            exact
-                            path={url}
-                            key={key}
-                            role={role}
-                            withLayout={withLayout}
-                        />
-                    );
-                })} */}
+                <PrivateRoute path="/master/product" component={MasterProduct} withLayout />
                 <PrivateRoute component={ErrorPage} withLayout />
             </Switch>
         </BrowserRouter>
--- logistic/src/app/store.js
+++ logistic/src/app/store.js
@@ -1,12 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "features/authentication/authSlice";
-import counterReducer from "features/counter/counterSlice";
+import productReducer from "features/master/Product/masterProductSlice";
 import componentReducer from "infrastructure/components/componentSlice";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from "redux-persist";
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
+import storage from "./syncStorage";
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    generalComponent: componentReducer,
+    masterProduct: productReducer
+});
+
+const rootPersistConfig = {
+    key: "root",
+    storage,
+    stateReconciler: hardSet
+};
+
+export const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+            }
+        })
+    ]
+});
+export const persistor = persistStore(store);
+
+export const originalStore = configureStore({
     reducer: {
         auth: authReducer,
-        counter: counterReducer,
-        generalComponent: componentReducer
+        generalComponent: componentReducer,
+        masterProduct: productReducer
     }
 });
