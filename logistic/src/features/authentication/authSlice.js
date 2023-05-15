9395d7f40dc12ccba7737b9fc1496c02d44c51ac
abishalih <habibullahshalihuddin@gmail.com>
2023-05-15 08:54:58 +0700

    feature: create signIn function and enhance signOut function

--- logistic/src/features/authentication/authSlice.js
+++ logistic/src/features/authentication/authSlice.js
@@ -1,51 +1,40 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchCount } from "./authAPI";
+import { createSlice } from "@reduxjs/toolkit";
+import { deleteAuthToken } from "infrastructure/utils/useAuthToken";
+import { setStorage } from "infrastructure/utils/useRoutes";
+import jwtEncode from "jwt-encode";
 
 const initialState = {
-    userInformation: {
-        email: "",
-        iss: "",
-        menu: [],
-        name: "",
-        poolCode: "",
-        realToken: "",
-        roleDesc: ""
-    },
+    userInformation: { username: "" },
     isLogin: false,
     status: "idle"
 };
-
-export const incrementAsync = createAsyncThunk("auth/fetchCount", async (amount) => {
-    const response = await fetchCount(amount);
-    return response.data;
-});
 
 export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        incrementByAmount: (state, action) => {
-            state.value += action.payload;
+        signIn: (state, action) => {
+            const { redirectUri, ...payload } = action.payload;
+            state.userInformation = payload;
+            state.isLogin = true;
+
+            const secret = "secret";
+            const accessToken = jwtEncode(payload, secret);
+            setStorage(accessToken, "");
+
+            console.log({ payload, accessToken, redirectUri });
+            window.location.replace(redirectUri);
         },
         signOut: (state) => {
             state.userInformation = initialState.userInformation;
             state.isLogin = false;
+            deleteAuthToken();
+            location.reload();
         }
-    },
-
-    extraReducers: (builder) => {
-        builder
-            .addCase(incrementAsync.pending, (state) => {
-                state.status = "loading";
-            })
-            .addCase(incrementAsync.fulfilled, (state, action) => {
-                state.status = "idle";
-                state.value += action.payload;
-            });
     }
 });
 
-export const { incrementByAmount, signOut } = authSlice.actions;
+export const { signIn, signOut } = authSlice.actions;
 
 export const selectIsLogin = (state) => state.auth.isLogin;
 export const selectUserInformation = (state) => state.auth.userInformation;
