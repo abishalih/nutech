b4caedfd509a8fa49c52bb63ef21d63da8e5d96f
abishalih <habibullahshalihuddin@gmail.com>
2023-05-15 08:55:19 +0700

    feature: add login route

--- logistic/src/app/App.js
+++ logistic/src/app/App.js
@@ -1,6 +1,7 @@
 import PrivateRoute from "app/routes/PrivateRoute";
 import ErrorPage from "infrastructure/templates/ErrorPage";
 import Home from "pages/Home";
+import Login from "pages/Login";
 import MasterProduct from "pages/master/Product";
 import { BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
@@ -10,6 +11,7 @@ function App() {
         <BrowserRouter>
             <Switch>
                 <PrivateRoute path="/" component={Home} withLayout exact />
+                <PrivateRoute path="/login" component={Login} />
                 <PrivateRoute path="/master/product" component={MasterProduct} withLayout />
                 <PrivateRoute component={ErrorPage} withLayout />
             </Switch>
