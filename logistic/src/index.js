1eae5a01b5607c225585a96eb7daafb1e348baed
abishalih <habibullahshalihuddin@gmail.com>
2023-05-16 11:06:54 +0700

    feature: change originalStore to store

--- logistic/src/index.js
+++ logistic/src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./app/App";
-import { originalStore, persistor } from "./app/store";
+import { persistor, store } from "./app/store";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
@@ -13,7 +13,7 @@ const root = createRoot(container);
 
 root.render(
     <StrictMode>
-        <Provider store={originalStore}>
+        <Provider store={store}>
             <PersistGate loading={LoadingPage} persistor={persistor}>
                 <App />
             </PersistGate>
