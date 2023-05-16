9307abe5f4d6385e114ccff2c3329c821b2be956
abishalih <habibullahshalihuddin@gmail.com>
2023-05-16 11:03:14 +0700

    fix: remove destruct object since selector already detailed

--- logistic/src/infrastructure/utils/useUserInfo.js
+++ logistic/src/infrastructure/utils/useUserInfo.js
@@ -1,9 +1,8 @@
 import { selectUserInformation } from "features/authentication/authSlice";
 import { useSelector } from "react-redux";
-import { generateCamelKey } from "./useCustomCase";
 
 export const useUserInfo = () => {
-    const { userInformation = {} } = useSelector(selectUserInformation);
-    const { username = "" } = generateCamelKey(userInformation);
+    const userInformation = useSelector(selectUserInformation);
+    const { username = "" } = userInformation;
     return { username };
 };
